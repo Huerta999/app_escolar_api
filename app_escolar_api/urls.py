2cde5bf661d63d3689a35869eb128b172dfb324a
donaji <145634056+Huerta999@users.noreply.github.com>
2025-12-05 11:42:11 -0600

    Update urls.py

--- app_escolar_api/urls.py
+++ app_escolar_api/urls.py
@@ -1,38 +1,31 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-from app_escolar_api.views import users, alumnos, maestros, auth
-from app_escolar_api.views.materias import MateriasAll, MateriasView, VerificarNrcView
-from app_escolar_api.views.users import TotalUsers
+from app_escolar_api.views import  materias, users, alumnos, maestros, auth
 
 urlpatterns = [
-
-    # --- ADMIN USERS ---
-    path("admin/", users.AdminView.as_view()),
-    path("lista-admins/", users.AdminAll.as_view()),
-
-    # --- ALUMNOS ---
-    path("alumnos/", alumnos.AlumnosView.as_view()),
-    path("lista-alumnos/", alumnos.AlumnosAll.as_view()),
-
-    # --- MAESTROS ---
-    path("maestros/", maestros.MaestrosView.as_view()),
-    path("lista-maestros/", maestros.MaestrosAll.as_view()),
-
-    # --- MATERIAS ---
-    path("materias-all/", MateriasAll.as_view()),
-    path("materias/", MateriasView.as_view()),
-    path("materias/<int:id>/", MateriasView.as_view()),
-    path("materias/verificar-nrc/<str:nrc>/", VerificarNrcView.as_view()),
-
-    # --- ESTADÍSTICAS ---
-    path("total-users/", TotalUsers.as_view()),
-
-    # --- AUTH ---
-    path("login/", auth.CustomAuthToken.as_view()),
-    path("logout/", auth.Logout.as_view()),
+    # Create Admin
+    path('admin/', users.AdminView.as_view()),
+    # Admin Data
+    path('lista-admins/', users.AdminAll.as_view()),
+    # Create Alumno
+    path('alumnos/', alumnos.AlumnosView.as_view()),
+    path('lista-alumnos/', alumnos.AlumnosAll.as_view()),
+    # Create Maestro
+    path('maestros/', maestros.MaestrosView.as_view()),
+    # Listar Maestros
+    path('lista-maestros/', maestros.MaestrosAll.as_view()),
+    # Total de usuarios
+    path('total-usuarios/', users.TotalUsers.as_view()),
+    # Login
+    path('login/', auth.CustomAuthToken.as_view()),
+    # Logout
+    path('logout/', auth.Logout.as_view()),
+    # Materias
+    path('lista-materias/', materias.MateriasAll.as_view()),
+    path('materias/', materias.MateriasView.as_view()),
+    path('materias/<int:id>/', materias.MateriasView.as_view()),
+    path('materias/verificar-nrc/<str:nrc>/', materias.VerificarNrcView.as_view()),
 ]
 
 if settings.DEBUG:
