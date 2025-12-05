98da2f8eab0110840475ee10ee61e100a26c31c7
donaji <donajihuerta555@gmail.com>
2025-12-04 23:30:35 -0600

    Fix Django URLS

---
+++ urls.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from app_escolar_api.views import users, alumnos, maestros, auth
+from app_escolar_api.views.materias import MateriasAll, MateriasView, VerificarNrcView
+from app_escolar_api.views.users import TotalUsers
+
+urlpatterns = [
+    # Admin
+    path('admin/', users.AdminView.as_view()),
+    path('lista-admins/', users.AdminAll.as_view()),
+
+    # Alumnos
+    path('alumnos/', alumnos.AlumnosView.as_view()),
+    path('lista-alumnos/', alumnos.AlumnosAll.as_view()),
+
+    # Maestros
+    path('maestros/', maestros.MaestrosView.as_view()),
+    path('lista-maestros/', maestros.MaestrosAll.as_view()),
+
+    # Total Usuarios
+    path('total-usuarios/', TotalUsers.as_view()),
+
+    # Materias
+    path('materias-all/', MateriasAll.as_view()),
+    path('materias/', MateriasView.as_view()),
+    path('materias/<int:id>/', MateriasView.as_view()),
+    path('materias/verificar-nrc/<str:nrc>/', VerificarNrcView.as_view()),
+
+    # Login
+    path('login/', auth.CustomAuthToken.as_view()),
+    path('logout/', auth.Logout.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
