d23e0c2b021fe1bf61d672b7cebbfc5c0a0cb5f2
donaji <145634056+Huerta999@users.noreply.github.com>
2025-12-05 12:07:00 -0600

    Update settings.py

--- app_escolar_api/settings.py
+++ app_escolar_api/settings.py
@@ -119,8 +119,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        'rest_framework.authentication.SessionAuthentication',
+        'app_escolar_api.models.BearerTokenAuthentication',
     ),
 
     "DEFAULT_FILTER_BACKENDS": [
