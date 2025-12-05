118bcb1ffdfb99bd0a87c923ffa1081657b1663d
donaji <donajihuerta555@gmail.com>
2025-12-04 23:01:34 -0600

    Fix whitenoise missing dependency

--- app_escolar_api/urls.py
+++ app_escolar_api/urls.py
@@ -2,45 +2,32 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views.bootstrap import VersionView
-from app_escolar_api.views import bootstrap
-from app_escolar_api.views import users
-from app_escolar_api.views import alumnos
-from app_escolar_api.views import maestros
-from app_escolar_api.views import auth
-from app_escolar_api.views.materias import MateriasAll, MateriasView, VerificarNrcView
-from app_escolar_api.views.users import TotalUsers 
 
+from app_escolar_api.views import users, alumnos, maestros, auth
+from app_escolar_api.views.materias import MateriasAll, MateriasView, VerificarNrcView
+from app_escolar_api.views.users import TotalUsers
 
 urlpatterns = [
-    #Create Admin
-        path('admin/', users.AdminView.as_view()),
-    #Admin Data
-        path('lista-admins/', users.AdminAll.as_view()),
-    #Edit Admin
-        #path('admins-edit/', users.AdminsViewEdit.as_view())
-     #Create Alumno
-        path('alumnos/', alumnos.AlumnosView.as_view()),
-    #Listar Alumno
-        path('lista-alumnos/', alumnos.AlumnosAll.as_view()),
-    #Create Maestro
-        path('maestros/', maestros.MaestrosView.as_view()),
-    #Listar Maestros
-        path('lista-maestros/', maestros.MaestrosAll.as_view()),
-    #Total de usuarios
-        path('total-usuarios/', users.TotalUsers.as_view()),
-    # Materias
-        path('materias-all/', MateriasAll.as_view()),                  
-        path('materias/', MateriasView.as_view()),                     
-        path('materias/<int:id>/', MateriasView.as_view()),             
-        path('materias/verificar-nrc/<str:nrc>/', VerificarNrcView.as_view()),
-    # Gráficas 
-        path('total-users/', TotalUsers.as_view()),
-        # Login
-        path('login/', auth.CustomAuthToken.as_view()),
-        # Logout
-        path('logout/', auth.Logout.as_view()),
+    path("api/", include([
+        path("admin/", users.AdminView.as_view()),
+        path("lista-admins/", users.AdminAll.as_view()),
 
+        path("alumnos/", alumnos.AlumnosView.as_view()),
+        path("lista-alumnos/", alumnos.AlumnosAll.as_view()),
+
+        path("maestros/", maestros.MaestrosView.as_view()),
+        path("lista-maestros/", maestros.MaestrosAll.as_view()),
+
+        path("materias-all/", MateriasAll.as_view()),
+        path("materias/", MateriasView.as_view()),
+        path("materias/<int:id>/", MateriasView.as_view()),
+        path("materias/verificar-nrc/<str:nrc>/", VerificarNrcView.as_view()),
+
+        path("total-users/", TotalUsers.as_view()),
+
+        path("login/", auth.CustomAuthToken.as_view()),
+        path("logout/", auth.Logout.as_view()),
+    ])),
 ]
 
 if settings.DEBUG:
